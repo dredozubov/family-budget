61fe05dd05cf6e6b1be0928c3b6d6e1edb5f8ca8
Denis Redozubov <denis.redozubov@gmail.com>
2012-12-10 13:42:37 +0400

    iOS version macroses

---
+++ familyFinance/Macros.h
@@ -0,0 +1,18 @@
+//
+//  Macros.h
+//  familyFinance
+//
+//  Created by Denis Redozubov on 05.12.12.
+//  Copyright (c) 2012 Denis Redozubov. All rights reserved.
+//
+
+#ifndef familyFinance_Macros_h
+#define familyFinance_Macros_h
+#define IOS_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define IOS_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define IOS_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+
+#endif
