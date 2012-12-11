d86e8a354ad867577b9bf2da9dfc933239dd2351
Denis Redozubov <denis.redozubov@gmail.com>
2012-12-11 14:37:25 +0400

    GHUnit testing framework built in another target

---
+++ familyFinance/GHTests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GHTests
+//
+//  Created by Denis Redozubov on 11.12.12.
+//  Copyright (c) 2012 Denis Redozubov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
